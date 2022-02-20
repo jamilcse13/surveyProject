675e615569bc347ac50d7df7cb08daf9cb90f244
jamil <jamil.cse13@gmail.com>
2022-02-20 14:28:12 +0600

    make and run table migration

--- surveyApp/admin.py
+++ surveyApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . models import Survey, Customer, Question, Answer, Option
 
 # Register your models here.
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
--- surveyApp/models.py
+++ surveyApp/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+class Survey(models.Model):
+    title = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ['title']
+    
+    def __str__(self):
+        return f"{self.title}"
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    surveys = models.ManyToManyField(Survey)
+
+    class Meta:
+        ordering = ['first_name']
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Question(models.Model):
+    question = models.CharField(max_length=150)
+    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['question']
+    
+    def __str__(self):
+        return f"{self.question}, {self.survey_id}"
+
+class Option(models.Model):
+    name = models.CharField(max_length=100)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return f"{self.name}, {self.question_id}"
+
+class Answer(models.Model):
+    option_id = models.ForeignKey(Option, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['option_id']
+    
+    def __str__(self):
+        return f"{self.option_id}, {self.customer_id}"
