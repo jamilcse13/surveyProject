3347cd76e42147d03d97560073fcda43deae9959
jamil <jamil.cse13@gmail.com>
2022-02-20 11:19:44 +0600

    connected App with Project and run demo api

--- surveyApp/views.py
+++ surveyApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def Customer(request):
+    mydictionary = {
+        "name" : 'John Doe',
+        "age" : 30
+    }
+    return JsonResponse(mydictionary)
